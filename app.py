dcc5b9aea2af03f4c678eb77e24d09cbd8aa23be
rajkumar472 <rajkumarlalwani813@gmail.com>
2025-06-16 13:04:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-# Triggering deploy from EC2
-
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "return "Updated version deployed from GitHub!"
+    return "Updated version deployed from GitHub!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
